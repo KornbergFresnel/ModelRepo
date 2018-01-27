7ffd2c4d683d8b25a0d77feb65eb288050f13e79
Andrew Kornberg <kornbergfresnel@outlook.com>
2018-01-27 16:44:36 +0800

    add normalize layer

--- MADDPG/model.py
+++ MADDPG/model.py
@@ -3,6 +3,7 @@ import os
 import tensorflow as tf
 import numpy as np
 import tensorflow.contrib.distributions as tcd
+import tensorflow.contrib as tc
 from config import GeneralConfig
 from tools import Record
 
@@ -52,7 +53,7 @@ class Actor(BaseModel):
 
             with tf.variable_scope("Optimization"):
                 self.q_gradient = tf.placeholder(tf.float32, shape=(None, self.action_space.n), name="Q-gradient")
-                self.policy_gradients = tf.gradients(self.e_out, self.e_variables, -1.0 * self.q_gradient)
+                self.policy_gradients = tf.gradients(self.e_out, self.e_variables, self.q_gradient)
                 self.train_op = tf.train.AdamOptimizer(self.learning_rate).apply_gradients(
                     zip(self.policy_gradients, self.e_variables)
                 )
@@ -61,12 +62,16 @@ class Actor(BaseModel):
                 self.update_op = [tf.assign(self.t_variables[i], self.decay * self.e_variables[i] + (1 - self.decay) * self.t_variables[i])
                                   for i in range(len(self.t_variables))]
 
-    def _construct(self, out_dim, name_scope):
+    def _construct(self, out_dim, name_scope, norm=True):
         l1 = tf.layers.dense(self.obs_input, units=self.layers_conf[0], activation=self.active_func,
                              kernel_initializer=self.initialize, name="l1")
+        if norm: l1 = tc.layers.layer_norm(l1)
+
         l2 = tf.layers.dense(l1, units=self.layers_conf[1], activation=self.active_func,
                              kernel_initializer=self.initialize, name="l2")
-        out = tf.layers.dense(l2, activation=tf.nn.softmax, units=out_dim, kernel_initializer=self.initialize, use_bias=True)
+        if norm: l2 = tc.layers.layer_norm(l2)
+
+        out = tf.layers.dense(l2, activation=tf.nn.softmax, units=out_dim, kernel_initializer=self.initialize)
         # out = tcd.RelaxedOneHotCategorical(self.temperature, probs=out).sample()
         variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name_scope)
 
@@ -75,28 +80,11 @@ class Actor(BaseModel):
     def act(self, obs_set):
         obs_set = obs_set.reshape((1,) + obs_set.shape)
         policy = self.sess.run(self.e_out, feed_dict={self.obs_input: obs_set})
-        # idx = np.argmax(policy[0])
-        #
-        # if self.env.discrete_action_input:
-        #     action = np.zeros(self.action_space.n, dtype=np.int32)
-        #     action[idx] = 1
-        # else:
-        #     action = np.zeros(self.action_space.n, dtype=np.float32)
-        #     action[idx] += 1.
         return policy[0]
 
     def target_act(self, obs_set):
         """Return an action id -> integer"""
-        # obs_set = obs_set.reshape((1,) + obs_set.shape)
         policy = self.sess.run(self.e_out, feed_dict={self.obs_input: obs_set})
-        # idx = np.argmax(policy, axis=1)
-        #
-        # if self.env.discrete_action_input:
-        #     action = np.zeros((len(policy), self.action_space.n), dtype=np.int32)
-        #     action[idx] = 1
-        # else:
-        #     action = np.zeros((len(policy), self.action_space.n), dtype=np.float32)
-        #     action[idx] += 1.
         return policy
 
     def update(self):
@@ -108,10 +96,6 @@ class Actor(BaseModel):
             self.q_gradient: action_gradients
         })
 
-        # variables = self.sess.run(self._variables)
-        # if np.isnan(variables[0]).any():  # I don't know how to use RelaxedOneHotCategorical, so duprecate it
-        #     input("FUCKYOU FEEFEF")
-
 
 class Critic(BaseModel):
     def __init__(self, env, sess, name, agent_id, config):
@@ -121,7 +105,6 @@ class Critic(BaseModel):
         self.env = env
         self.agent_id = agent_id
 
-        # self.action_space = env.action_space[agent_id]  # private action_space
         # flatten observation shape
         self.mul_obs_dim = (sum([len(env.observation_callback(env.agents[i], env.world)) for i in range(env.n)]),)
         # flatten action shape
@@ -160,16 +143,18 @@ class Critic(BaseModel):
                 self.t_q_input = tf.placeholder(tf.float32, shape=(None, 1), name="target-input")
                 self.loss = 0.5 * tf.reduce_mean(tf.square(self.t_q_input - self.e_q)) + weight_decay
                 self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
-                self.action_gradients = tf.gradients(self.e_q, self.mul_act_input)
+                self.action_gradients = tf.gradients(-tf.reduce_mean(self.e_q), self.mul_act_input)
 
-    def _construct(self, name_scope):
+    def _construct(self, name_scope, norm=True):
         l1 = tf.layers.dense(self.input, units=self.layers_conf[0], activation=self.active_func,
                              kernel_initializer=self.initial, name="l1")
+        if norm: l1 = tc.layers.layer_norm(l1)
+
         l2 = tf.layers.dense(l1, units=self.layers_conf[1], activation=self.active_func,
                              kernel_initializer=self.initial, name="l2")
-        # l3 = tf.layers.dense(l2, units=self.layers_conf[2], activation=self.active_func, name="l3")
-        q = tf.layers.dense(l2, units=1, use_bias=False,
-                            kernel_initializer=self.initial, name="Q")
+        if norm: l2 = tc.layers.layer_norm(l2)
+
+        q = tf.layers.dense(l2, units=1, kernel_initializer=self.initial, name="Q")
         
         variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name_scope)
 
@@ -189,19 +174,20 @@ class Critic(BaseModel):
         self.sess.run(self.update_op)
     
     def get_action_graidents(self, obs, action):
-        return self.sess.run(self.action_gradients, feed_dict={
+        action_gradients, e_q = self.sess.run([self.action_gradients, self.e_q], feed_dict={
             self.mul_obs_input: obs,
             self.mul_act_input: action
-        })[0]
+        })
+        return action_gradients[0], np.mean(e_q)
 
     def train(self, target_q_values, obs, action):
-        _, loss, e_q = self.sess.run([self.train_op, self.loss, self.e_q], feed_dict={
+        _, loss = self.sess.run([self.train_op, self.loss], feed_dict={
             self.mul_obs_input: obs,
             self.mul_act_input: action,
             self.t_q_input: target_q_values.reshape((target_q_values.shape[0], 1)),
         })
 
-        return np.mean(e_q), loss
+        return loss
 
 
 class MultiAgent(object):
@@ -220,6 +206,7 @@ class MultiAgent(object):
         self.update_every = _CONFIG.update_every
         self.train_freq = _CONFIG.train_freq
 
+
         # == Construct Network for Each Agent ==
         with tf.variable_scope(self.name):
             for agent_id in range(self.env.n):
@@ -228,19 +215,29 @@ class MultiAgent(object):
                     self.critic.append(Critic(env, self.sess, name, agent_id, _CONFIG))
 
                     self.actions_dims.append(self.env.action_space[agent_id].n)
+        
+            # === Define summary ===
+            self.reward = [tf.placeholder(tf.float32, shape=None, name="Agent_{}_reward".format(i)) for i in range(self.env.n)]
+            self.reward_op = [tf.summary.scalar('Agent_{}_R_Mean'.format(i), self.reward[i]) for i in range(self.env.n)]
 
         self.sess.run(tf.global_variables_initializer())
+        self.train_writer = tf.summary.FileWriter(_CONFIG.log_dir, graph=tf.get_default_graph())
 
         for i in range(self.env.n):
             self.actor[i].update()
             self.critic[i].update()
     
-    def act(self, obs_set):
+    def act(self, obs_set, noise=0.0):
         """Accept a observation list, return action list of all agents."""
         actions = []
         for i, obs in enumerate(obs_set):
-            actions.append(self.actor[i].act(obs))
+            n = self.actions_dims[i]
+            actions.append(self.actor[i].act(obs) + np.random.randn(n) * noise)
         return actions
+
+    def summary(self, rewards, step):
+        for i in range(self.env.n):
+            self.train_writer.add_summary(self.sess.run(self.reward_op[i], feed_dict={self.reward[i]: np.mean(rewards[i])}), global_step=step)
 
     def restructure(self, data):
         """Restructure in-data for graidients apply
@@ -265,6 +262,11 @@ class MultiAgent(object):
             begin += len_act
 
         return new_data
+
+    def update_nets(self):
+        for j in range(self.env.n):
+            self.actor[j].update()
+            self.critic[j].update()
 
     def save(self, dir_path, epoch):
         """Save model
@@ -302,55 +304,52 @@ class MultiAgent(object):
 
     def train(self, replay_buffer):
         """Training logical"""
-        n_buffer = len(replay_buffer)
-        n_batch = (n_buffer + replay_buffer.batch_size - 1) // replay_buffer.batch_size * self.train_freq
+        # n_buffer = len(replay_buffer)
+        # n_batch = (n_buffer + replay_buffer.batch_size - 1) // replay_buffer.batch_size * self.train_freq
+        n_batch = 1
         action_gradients = [[] for _ in range(self.env.n)]
 
         start = time.time()
 
         self.record = Record(self.env.n)
 
-        for i_batch in range(n_batch):
-            batch_data = replay_buffer.sample()  # for all agents
+        # for i_batch in range(n_batch):
+        batch_data = replay_buffer.sample()  # for all agents
 
-            # data with `clus` prefix is prepared for critic
-            obs_clus = np.concatenate(batch_data.obs, axis=1)
-            obs_next_clus = np.concatenate(batch_data.obs_next, axis=1)
-            action_clus = np.concatenate(batch_data.action, axis=1)
-            batch_next_action = []
+        # data with `clus` prefix is prepared for critic
+        obs_clus = np.concatenate(batch_data.obs, axis=1)
+        obs_next_clus = np.concatenate(batch_data.obs_next, axis=1)
+        action_clus = np.concatenate(batch_data.action, axis=1)
+        batch_next_action = []
 
-            for j in range(self.env.n):
-                batch_next_action.append(self.actor[j].target_act(batch_data.obs_next[j]))
+        for j in range(self.env.n):
+            batch_next_action.append(self.actor[j].target_act(batch_data.obs_next[j]))
 
-            batch_next_action = np.concatenate(batch_next_action, axis=1)
+        batch_next_action = np.concatenate(batch_next_action, axis=1)
 
-            for j in range(self.env.n):  # calculate Q-value for (s_next, a_next)
-                batch_q_value = self.critic[j].calculate_target_q(obs_next_clus, batch_next_action)
-                batch_q_value = batch_data.reward[:, j] + (1. - batch_data.terminate[:, j]) * batch_q_value
+        for j in range(self.env.n):  # calculate Q-value for (s_next, a_next)
+            batch_q_value = self.critic[j].calculate_target_q(obs_next_clus, batch_next_action)
+            batch_q_value = batch_data.reward[:, j] + (1. - batch_data.terminate[:, j]) * batch_q_value
 
-                # then training critic, return with eval-Q & loss
-                eval_q, critic_loss = self.critic[j].train(batch_q_value, obs_clus, action_clus)
-                # compute action_gradients
-                action_gradients[j] = self.critic[j].get_action_graidents(obs_next_clus, batch_next_action)
+            # then training critic, return with eval-Q & loss
+            critic_loss = self.critic[j].train(batch_q_value, obs_clus, action_clus)
+            # compute action_gradients
+            action_gradients[j], mean_q = self.critic[j].get_action_graidents(obs_next_clus, batch_next_action)
 
-                self.record.reward[j] += eval_q
-                self.record.loss[j] += critic_loss
-            
-            # reshape action_gradients for each agents
-            action_gradients = self.restructure(np.array(action_gradients) / self.env.n)
+            self.record.reward[j] += mean_q
+            self.record.loss[j] += critic_loss
+        
+        # reshape action_gradients for each agents
+        action_gradients = self.restructure(np.array(action_gradients))
 
-            for j in range(self.env.n):
-                # then training actor: action_gradients should come from other agents, not only one
-                self.actor[j].train(batch_data.obs_next[j], action_gradients[j])
+        for j in range(self.env.n):
+            # then training actor: action_gradients should come from other agents, not only one
+            self.actor[j].train(batch_data.obs_next[j], action_gradients[j])
 
-            # variables = self.sess.run(self.actor[1].e_variables)
-            # if np.isnan(variables[0]).any():
-            #     input("Fuck You")
-
-            if (i_batch + 1) % self.update_every:  # update and make summaries
-                for j in range(self.env.n):
-                    self.actor[j].update()
-                    self.critic[j].update()
+            # if (i_batch + 1) % self.update_every:  # update and make summaries
+            #     for j in range(self.env.n):
+            #         self.actor[j].update()
+            #         self.critic[j].update()
 
         end = time.time()
         total_reward = np.around(np.array(self.record.reward) / n_batch, decimals=6)
